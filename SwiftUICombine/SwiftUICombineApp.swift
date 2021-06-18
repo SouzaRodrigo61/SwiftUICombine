7a87967380728c4196cf6da93e0a09d0e19b9665
Rodrigo Santos <souza.rodrigo61@gmail.com>
2021-06-18 10:54:45 -0300

    :tada: project(combine): initial project

---
+++ SwiftUICombine/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  SwiftUICombine
+//
+//  Created by Rodrigo Santos on 18/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
